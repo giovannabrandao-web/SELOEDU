f08ee34af3b712a2f980e4f7680fe83a0a6262c9
Giovanna Rodrigues Brandão Viana <giovannarodriguesbviana@gmail.com>
2025-09-18 23:01:39 -0300

    Segundo commit

--- app.py
+++ app.py
@@ -1,24 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
-users = [
-    {"id": "1", "nome": "Mauro", "email": "mauro@gmail.com", "perfil": "Coordenador", "status": "Ativo"},
-    {"id": "2", "nome": "Giovanna", "email": "giovanna@gmail.com", "perfil": "Aluno", "status": "Ativo"},
-    {"id": "3", "nome": "Gabriel", "email": "gabriel@gmail.com", "perfil": "Aluno", "status": "Ativo"},
-    {"id": "4", "nome": "Miguel", "email": "miguel@gmail.com", "perfil": "Aluno", "status": "Inativo"},
-    {"id": "5", "nome": "Vinicius", "email": "vinicius@gmail.com", "perfil": "Professor", "status": "Ativo"},
-    {"id": "6", "nome": "Zayra", "email": "zayra@gmail.com", "perfil": "Aluno", "status": "Ativo"},
-    {"id": "7", "nome": "Diógenes", "email": "diogenes@gmail.com", "perfil": "Professor", "status": "Inativo"}
-]
+@app.route("/home")
+def home():
+    return render_template("home.html")
 
-@app.route("/user/<nome>")
-def user_page(nome):
-    return render_template("user.html", nome=nome)
- 
-@app.route("/users")
-def list_users():
-    return render_template("users.html", users=users)
+@app.route("/login")
+def login():
+    return render_template("auth/login.html")
+
+@app.route("/do_login", methods=["POST"])
+def do_login():
+    return redirect(url_for("home"))
 
 if __name__ == "__main__":
     app.run(debug=True)
